c828608fb8f24571b5260527227b2d6db10cd989
sebastianusbara <sebastianus.bara@gmail.com>
2015-12-16 12:52:11 +0700

    Image Carousel

--- assets/js/main.min.js
+++ assets/js/main.min.js
@@ -19,6 +19,7 @@
             Site.fastClick();
             Site.enableActiveStateMobile();
             Site.WPViewportFix();
+            Site.imageSwitcher();
 
             window.Site = Site;
         },
@@ -46,6 +47,43 @@
                 style.appendChild(fix);
                 document.getElementsByTagName('head')[0].appendChild(style);
             }
+        },
+
+        imageSwitcher: function () {
+            var $gallery    = $('.gallery');
+            var $mainImg    = $('.gallery__main img');
+            var $thumbnail  = $('.gallery__thumbs__container');
+            var $thumbs     = $('.gallery__thumbs');
+            var $navLeft    = $('.gallery__nav.nav__left');
+            var $navRight   = $('.gallery__nav.nav__right');
+            var $first      = $('.gallery__thumbs').first();
+            var $last       = $('.gallery__thumbs').last();
+
+
+            $gallery.on('click', '.gallery__thumbs img', function(event) {
+                var $getImg = $(this).attr('src');
+                var $getAlt = $(this).attr('alt');
+                
+                $('.overlay').remove();
+                $mainImg.before('<div class="overlay"><img src="assets/img/loading.gif"/></div>');
+                $mainImg.attr({
+                    src: $getImg,
+                    alt: $getAlt
+                });
+                $('.overlay').delay(400).fadeOut('slow', function() { $(this).remove(); })
+            });
+
+            $navLeft.on('click', function() {
+                $thumbnail.animate({left: '-=145px'});
+                $last.remove().css({ 'margin-left': '5px' });
+                $last.after($first);
+            });
+
+            $navRight.on('click', function() {
+                $last.remove().css({ 'margin-left': '-145px' }).css({ 'margin-right': '5px' });
+                $thumbnail.animate({left: '+=145px'});
+                $first.before($last);
+            });
         }
 
     };
